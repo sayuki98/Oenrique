c7434cccc4318e64331030328ec32f000120f45e
sayuki98 <sayuki1998@gmail.com>
2019-09-04 04:20:32 +0300

    testimonials

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+
+    });
+});
